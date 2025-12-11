b58549e1d1d6e2a924e4003e56dea2af81841f27
Pedro Henrique Bergamo <pedrohb.dev@outlook.com>
2025-12-11 14:38:36 -0300

    🎉 Commit inicial

---
+++ README.md
@@ -0,0 +1,26 @@
+# Better Auth Playground
+
+## Features
+
+- [ ] Adding Shadcn
+- [ ] Drizzle Setup
+- [ ] Better Auth Setup
+- [ ] Email Sign Up
+- [ ] Email Sign In
+- [ ] Arcjet Security Setup
+- [ ] OAuth Sign Up | Sign In
+- [ ] Auth Redirects
+- [ ] Email Verification
+- [ ] Forgot Password
+- [ ] Welcome Email (Better Auth Hooks)
+- [ ] Custom Database Fields
+- [ ] Profile Management Page
+- [ ] Change | Set Password Page
+- [ ] Session Management
+- [ ] Account Linking
+- [ ] Delete User Accounts
+- [ ] Two Factor Auth Plugin
+- [ ] Passkey Plugin
+- [ ] Admin Plugin
+- [ ] Organization Plugin
+- [ ] Stripe Plugin
---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import type { ReactNode } from "react";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Better Auth Playground",
+    template: "%s | Better Auth Playground",
+  },
+  description:
+    "Um playground para experimentar melhores estratégias de autenticação com Better Auth.",
+};
+
+interface IRootLayout {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: Readonly<IRootLayout>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
